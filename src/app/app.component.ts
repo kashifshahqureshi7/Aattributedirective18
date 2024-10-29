a5d550fae0a12e5977dbf6e38d9f4758afc0dce3
Muhammad Hassan <mhassan4641@gmail.com>
2024-10-29 18:05:47 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
+import { CommonModule, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'attributedirective';
+
+  mycountry = 'Pakistan';
+  
+  employees: any[] = [
+    { name: 'Kashif Shah', City: 'Dera Ismail Khan' },
+    { name: 'Asif Shah', City: 'Islamabad' },
+    { name: 'Saqib Shah', City: 'Peshawar' },
+    
+  ]
+  getcolor(City: any) {
+    switch (City) {
+      case 'Dera Ismail Khan':
+        return 'blue';
+      case 'Islamabad':
+        return 'red';
+      case 'Peshawar':
+        return 'pink';
+    }
+    return null
+    
+  }
 }
